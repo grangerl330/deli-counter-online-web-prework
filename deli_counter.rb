7da692ef66dca5fd51feaee988e177411bf76faa
Luke Granger <grangerl330@gmail.com>
2019-01-09 18:51:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,16 +12,15 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts ("Welcome, #{name}. You are number #{katz_deli.size} in line.")
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift()
+    puts "Currently serving #{katz_deli.shift}."
   end
 end 
   
